6b45c53b92928db19a409d1e4ac8deadc898aeeb
HobbyHacker <lfrenzel1@student.rccd.edu>
2025-10-20 16:29:16 -0700

    Feat: scaffolding the program structure

---
+++ project/main/main.ino
@@ -0,0 +1,50 @@
+/**
+ * @file main.ino
+ * @brief Clock program
+ * @author HobbyHacker
+ * @version 0.1.0
+ * @date 2025-10-20
+ *
+ * @details
+ * This is an experimental project to help me learn and better understand
+ * how a computer works by utilizing an ATmega and other integrated circuits
+ * to construct the basic computational components of a simple CPU system.
+ */
+
+/* ------------------------------------------------------------ */
+/*                        CLASS SECTION                         */
+/* ------------------------------------------------------------ */
+
+/**
+ * @class MachineState
+ * @brief Controls the overall flow of the machine.
+ * @details
+ * The MachineState class manages the current operational mode of the system,
+ * handling transitions such as Reset, Pause, Run, and Step. It acts as the
+ * central controller that determines how other subsystems (LEDs, buzzer,
+ * heartbeat, etc.) behave based on the active state.
+ */
+class MachineState
+{
+
+};
+
+/* ------------------------------------------------------------ */
+/*                      MAIN PROGRAM FLOW                       */
+/* ------------------------------------------------------------ */
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
+
+/* ------------------------------------------------------------ */
+/*                    CLASS IMPLEMENTATION                      */
+/* ------------------------------------------------------------ */
+
+
